485837251bd8fe0ac9a38fb5f49f7ac831d0fcb5
Ampersand-Costco <emersoncoskey@gmail.com>
2020-11-30 15:49:12 -0800

    tutorial 1.2 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //move the vehicle forward
-        transform.Translate(0, 0, 2);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
