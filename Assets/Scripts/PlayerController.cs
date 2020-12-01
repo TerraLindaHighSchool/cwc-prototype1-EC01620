7c72fb30ad1ca128af544250d9dd6ec3452add4e
Ampersand-Costco <emersoncoskey@gmail.com>
2020-11-30 18:23:36 -0800

    tutorial 1.3 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;
+
     // Start is called before the first frame update
     void Start()
     {
